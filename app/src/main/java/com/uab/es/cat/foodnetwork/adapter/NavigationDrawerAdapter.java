c945599523a94447928f592be81f2c7f32ba971f
ramonmacias <ramonmaciasgarcia@gmail.com>
2015-12-20 20:35:26 +0100

    Implementacion de recycler view con imagenes personalizadas para cada opcion, usuario donador y transportista

--- app/src/main/java/com/uab/es/cat/foodnetwork/adapter/NavigationDrawerAdapter.java
+++ app/src/main/java/com/uab/es/cat/foodnetwork/adapter/NavigationDrawerAdapter.java
@@ -1,13 +1,18 @@
 package com.uab.es.cat.foodnetwork.adapter;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.uab.es.cat.foodnetwork.ActionsFragment;
+import com.uab.es.cat.foodnetwork.EditProfileFragment;
 import com.uab.es.cat.foodnetwork.R;
+import com.uab.es.cat.foodnetwork.ViewProfileFragment;
 import com.uab.es.cat.foodnetwork.model.NavDrawerItem;
 
 import java.util.Collections;
@@ -43,6 +48,19 @@ public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDraw
     public void onBindViewHolder(MyViewHolder holder, int position) {
         NavDrawerItem current = data.get(position);
         holder.title.setText(current.getTitle());
+        switch (position) {
+            case 0:
+                holder.image.setImageResource(R.drawable.ic_insert_emoticon_black_24dp);
+                break;
+            case 1:
+                holder.image.setImageResource(R.drawable.ic_mode_edit_black_24dp);
+                break;
+            case 2:
+                holder.image.setImageResource(R.drawable.ic_apps_black_24dp);
+                break;
+            default:
+                break;
+        }
     }
 
     @Override
@@ -52,10 +70,12 @@ public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDraw
 
     class MyViewHolder extends RecyclerView.ViewHolder {
         TextView title;
+        ImageView image;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.title);
+            image = (ImageView) itemView.findViewById(R.id.icon_drawer);
         }
     }
 }
